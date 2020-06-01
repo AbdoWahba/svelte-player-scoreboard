43b2b2bcd7a347e782746b36b4cc845907b27012
AbdoWahba <abdo.tw20154@gmail.com>
2020-06-01 23:36:09 +0200

    adding addplayer and deleteplayer to the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from "./Navbar.svelte";
 	import Player from "./Player.svelte";
+	import AddPlayer from "./AddPlayer.svelte";
 	let players = [{
 			name: "Abdelrhman",
 			points: 100
@@ -14,12 +15,23 @@
 			points: 80
 		}
 	]
+
+	const addplayer = (e) => {
+		players = [...players, e.detail];
+	}
+	const deleteplayer = (e) => {
+		players = players.filter((player) => player.name != e.detail)
+	}
 </script>
 
-
 <Navbar />
+<AddPlayer on:addplayer={addplayer} />
 <div class="container">
+	{#if (players.length ===0)}
+	NO PLAYERS
+	{:else}
 	{#each players as player}
-	<Player name= {player.name} points={player.points}/>
+	<Player name= {player.name} points={player.points} on:deleteplayer={deleteplayer}/>
 	{/each}
+	{/if}
 </div>
--- src/Player.svelte
+++ src/Player.svelte
@@ -1,4 +1,8 @@
 <script>
+    import {
+        createEventDispatcher
+    } from "svelte";
+    const dispatch = createEventDispatcher();
     export let name;
     export let points;
     let showControlers = false;
@@ -11,6 +15,9 @@
     const toggleControlers = () => {
         showControlers = !showControlers
     }
+    const deletePlayer = () => {
+        dispatch("deleteplayer", name)
+    }
 </script>
 
 <style>
@@ -21,12 +28,15 @@
 <div class="card">
     <h1>{name}</h1>
     <h3>{points}</h3>
+    <div class="grid-5">
+        <button on:click={()=>{deletePlayer()}}>delete</button>
+        <button class="btn btn-sm btn-secondary " on:click={toggleControlers}>
+            {#if (showControlers)} - {:else} + {/if}
+    </button>
+    {#if (showControlers)}
+    <button type="button" class="btn btn-dark" on:click={removePoint}>-1</button>
+    <button type="button" class="btn" on:click={addPoint}>+1</button>
+    <input type="number" bind:value={points}>
+    {/if}
 </div>
-<button class="btn btn-sm btn-secondary " on:click={toggleControlers}>
-    {#if (showControlers)} - {:else} + {/if}
-		</button>
-		{#if (showControlers)}
-			<button type="button" class="btn btn-dark" on:click={removePoint}>-1</button>
-			<button type="button" class="btn" on:click={addPoint}>+1</button>
-			<input type="number" bind:value={points}>
-		{/if}
+</div>
