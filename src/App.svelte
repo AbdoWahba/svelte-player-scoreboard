f8e8d98b4274ed668961f8dd4e47cb9348f05850
AbdoWahba <abdo.tw20154@gmail.com>
2020-06-01 20:01:47 +0200

    adding another component and control statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,37 @@
 <script>
+	import Navbar from "./Navbar.svelte"
 	let name = "Abdelrhman tarek";
 	let points = 100;
+	let showControlers = false;
 	const addPoint = () => {
 		points += 1
 	}
 	const removePoint = () => {
 		points -= 1
 	}
+	const toggleControlers = () => {
+		showControlers = !showControlers
+	}
 </script>
 
 <style>
-
+	.u-m-b-5 {
+		margin-bottom: 50px;
+	}
 </style>
 
+<Navbar />
 <div class="container">
 	<div class="card">
 		<h1>{name}</h1>
 		<h3>{points}</h3>
+		<button class="btn btn-sm btn-secondary u-m-b-5" on:click={toggleControlers}>
+			{#if (showControlers)} + {:else} - {/if}
+		</button>
+		{#if (showControlers)}
 		<button class="btn" on:click={addPoint}>+1</button>
 		<button class="btn btn-dark" on:click={removePoint}>-1</button>
 		<input type="number" bind:value={points}>
+		{/if}
 	</div>
 </div>
---
+++ src/Navbar.svelte
@@ -0,0 +1,9 @@
+<style>
+    .u-m-b-5 {
+        margin-bottom: 50px;
+    }
+</style>
+
+<div class="navbar bg-primary u-m-b-5">
+    <h1>player scoreboard</h1>
+</div>
