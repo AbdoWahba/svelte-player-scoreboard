a7f5a14b74bfb301a3009729755d2aa4b467e464
AbdoWahba <abdo.tw20154@gmail.com>
2020-06-01 22:56:07 +0200

    separate player into single component and add multiple players

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,25 @@
 <script>
-	import Navbar from "./Navbar.svelte"
-	let name = "Abdelrhman tarek";
-	let points = 100;
-	let showControlers = false;
-	const addPoint = () => {
-		points += 1
-	}
-	const removePoint = () => {
-		points -= 1
-	}
-	const toggleControlers = () => {
-		showControlers = !showControlers
-	}
+	import Navbar from "./Navbar.svelte";
+	import Player from "./Player.svelte";
+	let players = [{
+			name: "Abdelrhman",
+			points: 100
+		},
+		{
+			name: "Tarek",
+			points: 90
+		},
+		{
+			name: "Wahba",
+			points: 80
+		}
+	]
 </script>
 
-<style>
-	.u-m-b-5 {
-		margin-bottom: 50px;
-	}
-</style>
 
 <Navbar />
 <div class="container">
-	<div class="card">
-		<h1>{name}</h1>
-		<h3>{points}</h3>
-		<button class="btn btn-sm btn-secondary u-m-b-5" on:click={toggleControlers}>
-			{#if (showControlers)} + {:else} - {/if}
-		</button>
-		{#if (showControlers)}
-		<button class="btn" on:click={addPoint}>+1</button>
-		<button class="btn btn-dark" on:click={removePoint}>-1</button>
-		<input type="number" bind:value={points}>
-		{/if}
-	</div>
+	{#each players as player}
+	<Player name= {player.name} points={player.points}/>
+	{/each}
 </div>
---
+++ src/Player.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let name;
+    export let points;
+    let showControlers = false;
+    const addPoint = () => {
+        points += 1
+    }
+    const removePoint = () => {
+        points -= 1
+    }
+    const toggleControlers = () => {
+        showControlers = !showControlers
+    }
+</script>
+
+<style>
+
+</style>
+
+
+<div class="card">
+    <h1>{name}</h1>
+    <h3>{points}</h3>
+</div>
+<button class="btn btn-sm btn-secondary " on:click={toggleControlers}>
+    {#if (showControlers)} - {:else} + {/if}
+		</button>
+		{#if (showControlers)}
+			<button type="button" class="btn btn-dark" on:click={removePoint}>-1</button>
+			<button type="button" class="btn" on:click={addPoint}>+1</button>
+			<input type="number" bind:value={points}>
+		{/if}
